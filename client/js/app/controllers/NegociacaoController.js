d5d7b3a44eac1244002653d720dc87e9460f7949
Rafael Herculano <rafa.lj61@gmail.com>
2018-10-10 21:23:43 -0300

    Improving performance through not making DOM requests

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,16 +1,21 @@
 class NegociacaoController{
 
-	adiciona(event){
+	constructor(){
+			
+		// aqui o bind serve pra que o querySelector mantenha a associação com o document
+		let $ = document.querySelector.bind(document);
+
+		this.inputData = $("#data");
+		this.inputQuantidade = $("#quantidade");
+		this.inputValor = $("#valor") ;
+	}
+
+	adiciona(event){		
 
 		event.preventDefault();
 
-		// aqui o bind serve pra que o querySelector mantenha a associação com o document
-		let $ = document.querySelector.bind(document);
-		
-		let inputData = $("#data");
-		let inputQuantidade = $("#quantidade");
-		let inputValor = $("#valor") ;
+		console.log(this.inputData.value) ;
+		console.log(this.inputQuantidade.value) ;
 
-		console.log(inputData.value) ;
 	}
 }
