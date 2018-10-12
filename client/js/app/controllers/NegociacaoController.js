8575621167dfa95f473bc1faf5e7d688d76dd872
Rafael Herculano <rafa.lj61@gmail.com>
2018-10-11 21:43:29 -0300

    Creating Negociacao List model and adding methods to NegociacaoController

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -8,25 +8,33 @@ class NegociacaoController{
 		this._inputData = $("#data");
 		this._inputQuantidade = $("#quantidade");
 		this._inputValor = $("#valor") ;
+		this._listaNegociacoes = new ListaNegociacoes() ;
 	}
 
 	adiciona(event){		
 
 		event.preventDefault();
 
-		let data = DateHelper.textoParaData(this._inputData.value) ;
+		this._listaNegociacoes.adiciona(this._criaNegociacao()) ;
+		this._limpaFormulario();
+		
+		console.log(this._listaNegociacoes.negociacoes);
+	}
 
-		console.log(data) ;
+	_criaNegociacao(){
+		return new Negociacao(
+				DateHelper.textoParaData(this._inputData.value),
+				this._inputQuantidade.value,
+				this._inputValor.value
+			); 
+	}
 
-		let negociacao = new Negociacao(
-			data,
-			this._inputQuantidade.value,
-			this._inputValor.value
-		); 
+	_limpaFormulario(){
 
-		let diaMesAno = DateHelper.dataParaTexto(negociacao.data) ;
+		this._inputData.value = '';
+		this._inputValor.value = 0.0 ; 
+		this._inputQuantidade.value = 1 ;
 
-		console.log(diaMesAno) ;
-
+		this._inputData.focus() ;
 	}
 }
